31981433b2ca47bb4c804b5b12eb6b0ca635dccd
Pragyan Tripathi <pragyan@clashapp.co>
2021-09-28 08:28:53 +0000

    add replica support

--- main.tf
+++ main.tf
@@ -155,7 +155,7 @@ resource "aws_db_subnet_group" "default" {
 resource "aws_security_group" "default" {
   count = module.this.enabled ? 1 : 0
 
-  name        = module.this.id
+  name        = var.replicate_source_db != null ? "${module.this.id}-replica" : module.this.id
   description = "Allow inbound traffic from the security groups"
   vpc_id      = var.vpc_id
   tags        = module.this.tags
